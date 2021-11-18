a6f00269962adb9a8628e1ab9086fa5083e699c8
Kaixianxian1214 <1678092258@qq.com>
2021-11-18 10:31:57 +0800

    Mac mini

---
+++ WebDemo3/src/main/java/com/webdemo3/controller/JspController.java
@@ -0,0 +1,14 @@
+package com.webdemo3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JspController {
+
+    @RequestMapping("/detail")
+    public String detail(){
+        return "detail";
+    }
+}
